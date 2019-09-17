8ce714362c1a9984340acc9029b42c37fca40e80
柏木孝洋 <kashiwagitakahiro@kashiwagitakahironoMacBook-Air.local>
2019-09-17 10:19:16 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+	<title>PROFILE</title>
+</head>
+<body>
+<!--ヘッド-->
+<div class="header">
+	<h1>PROFILE</h1>
+	<img class="chikin" src="img/f_1600_1200.jpg" alt="">
+</div>
+<!--ボディー-->
+<div class="main">
+	<div class="information">-基本情報-</div>
+
+	<ul class="name">
+		<li>名前　　柏木　孝洋</li>
+		<li>大阪府池田市（チキンラーメンの発祥地）</li>
+		<li>年齢　23歳</li>
+		<li>趣味　バスケットボール</li>
+		<li>人見知りなので皆さん仲良くしてください</li>
+	</ul>
+		
+	</div>	
+</div>
+
+<div>
+	<h2>趣味</h2>
+</div>
+<div class="imgline">
+	<div class="basket">
+		<a href="https://ja.wikipedia.org/wiki/%E3%82%B3%E3%83%BC%E3%83%93%E3%83%BC%E3%83%BB%E3%83%96%E3%83%A9%E3%82%A4%E3%82%A2%E3%83%B3%E3%83%88" target="_blank"><img src="img/39493.jpg" alt="">
+		</a>
+		<P>バスケットボール</P>
+	
+	<div>
+		<h2>三ヶ月間の目標</h2>
+		<p>簡単なウェブサイトを一から作れるようにすること</p>
+	</div>
+
+
+
+	<div class="footer">
+		<p>end</p>
+	</div>
+
+
+
+
+</body>
+</html>
